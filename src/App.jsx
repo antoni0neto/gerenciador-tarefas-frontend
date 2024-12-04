f2c8e7e31ab803b0d2ef3eb0f107a4c311c364c1
antoni0neto <antonio.silva6@yahoo.com.br>
2024-12-04 18:54:50 -0300

    feat: add react-router-dom setup and home page and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import Sidebar from "./components/Sidebar";
-import Tasks from "./components/Tasks";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.scss";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 const App = () => {
     return (
-        <div className="app-container">
-            <Sidebar />
-            <Tasks />
-        </div>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+        </Routes>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
 
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const options = {
     position: "top right",
@@ -18,9 +19,11 @@ const options = {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <AlertProvider template={AlertTemplate} {...options}>
-            <App />
-        </AlertProvider>
+        <BrowserRouter>
+            <AlertProvider template={AlertTemplate} {...options}>
+                <App />
+            </AlertProvider>
+        </BrowserRouter>
     </React.StrictMode>
 );
 
