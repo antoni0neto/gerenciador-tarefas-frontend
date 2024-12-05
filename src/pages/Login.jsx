9320f4c9961f8b30ad76cefd39c5b255a11fdf8e
antoni0neto <antonio.silva6@yahoo.com.br>
2024-12-05 10:26:02 -0300

    feat: add Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/images/logo-2.png";
+import CustomButton from "../components/CustomButton";
+
 import "./Login.scss";
 
 const Login = () => {
+    const navigate = useNavigate();
     return (
         <div className="login-container">
-            <h1>Login</h1>
+            <img src={logo} className="logo" alt="Full Stack Club" />
+            <CustomButton
+                className="button-container"
+                onClick={() => navigate("/")}
+            >
+                Entrar
+            </CustomButton>
         </div>
     );
 };
